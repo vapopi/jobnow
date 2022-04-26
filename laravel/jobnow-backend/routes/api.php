dcd1a779e9ce640bbc9c69c3462b0951cedaa20e
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-04-26 17:23:30 +0200

    api routes and NotificationController created

--- laravel/jobnow-backend/routes/api.php
+++ laravel/jobnow-backend/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\OfferController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('groups', GroupController::class);
+Route::apiResource('groups/{gid}/messages', MessageController::class);
+Route::apiResource('tickets', TicketController::class);
+Route::apiResource('tickets/{tid}/comments', CommentController::class);
+Route::apiResource('notifications', NotificationController::class);
+Route::apiResource('posts', PostController::class);
+Route::apiResource('offers', OfferController::class);
