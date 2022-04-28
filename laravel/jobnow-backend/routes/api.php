885ae7d88820aa16b8589fbe3b92313d43a22337
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-04-28 15:31:43 +0200

    routes middleware finished

--- laravel/jobnow-backend/routes/api.php
+++ laravel/jobnow-backend/routes/api.php
@@ -35,42 +35,43 @@ Route::apiResource('posts', PostController::class);
 Route::apiResource('offers', OfferController::class);
 
 //Middleware rutas API Group
-Route::get('api/groups', 'GroupController@index')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::post('api/groups', 'GroupController@store')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::get('api/groups/{group}', 'GroupController@show')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::put('api/groups/{group}', 'GroupController@update')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::delete('api/groups/{group}', 'GroupController@destroy')->middleware(['auth', 'roles: 1, 4, 5']);
+Route::get('api/groups', [GroupController::class, 'index'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::post('api/groups', [GroupController::class, 'store'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::get('api/groups/{group}', [GroupController::class, 'show'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::put('api/groups/{group}', [GroupController::class, 'update'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::delete('api/groups/{group}', [GroupController::class, 'destroy'])->middleware(['auth', 'roles: 1, 4, 5']);
 
 //Middleware rutas API Message
-Route::get('api/groups/{gid}/messages/{message}', 'MessageController@index')->middleware(['auth', 'roles: 1, 3, 4, 5']);
-Route::post('api/groups/{gid}/messages', 'MessageController@store')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::get('api/groups/{gid}/messages/{message}', 'MessageController@show')->middleware(['auth', 'roles: 1, 3, 4, 5']);
-Route::put('api/groups/{gid}/messages/{message}', 'MessageController@update')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::delete('api/groups/{gid}/messages/{message}', 'MessageController@destroy')->middleware(['auth', 'roles: 1, 3, 4, 5']);
+Route::get('api/groups/{gid}/messages/{message}', [MessageController::class, 'index'])->middleware(['auth', 'roles: 1, 3, 4, 5']);
+Route::post('api/groups/{gid}/messages', [MessageController::class, 'store'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::get('api/groups/{gid}/messages/{message}', [MessageController::class, 'show'])->middleware(['auth', 'roles: 1, 3, 4, 5']);
+Route::put('api/groups/{gid}/messages/{message}', [MessageController::class, 'update'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::delete('api/groups/{gid}/messages/{message}', [MessageController::class, 'destroy'])->middleware(['auth', 'roles: 1, 3, 4, 5']);
 
 //Middleware rutas API Ticket
-Route::get('api/tickets', 'TicketController@index')->middleware(['auth', 'roles: 1, 2']);
-Route::post('api/tickets', 'TicketController@store')->middleware(['auth', 'roles: 1, 5']);
-Route::get('api/tickets/{ticket}', 'TicketController@show')->middleware(['auth', 'roles: 1, 2, 5']);
-Route::put('api/tickets/{ticket}', 'TicketController@update')->middleware(['auth', 'roles: 1, 2, 5']);
-Route::delete('api/tickets/{ticket}', 'TicketController@destroy')->middleware(['auth', 'roles: 1']);
+Route::get('api/tickets', [TicketController::class, 'index'])->middleware(['auth', 'roles: 1, 2']);
+Route::post('api/tickets', [TicketController::class, 'store'])->middleware(['auth', 'roles: 1, 5']);
+Route::get('api/tickets/{ticket}', [TicketController::class, 'show'])->middleware(['auth', 'roles: 1, 2, 5']);
+Route::put('api/tickets/{ticket}', [TicketController::class, 'update'])->middleware(['auth', 'roles: 1, 2, 5']);
+Route::delete('api/tickets/{ticket}', [TicketController::class, 'destroy'])->middleware(['auth', 'roles: 1']);
 
 //Middleware rutas API Comment
-Route::get('api/tickets/{tid}/comments', 'CommentController@index')->middleware(['auth', 'roles: 2, 5']);
-Route::post('api/tickets/{tid}/comments', 'CommentController@store')->middleware(['auth', 'roles: 2, 5']);
-Route::get('api/tickets/{tid}/comments/{comment}', 'CommentController@show')->middleware(['auth', 'roles: 2, 5']);
-Route::put('api/tickets/{tid}/comments/{comment}', 'CommentController@update')->middleware(['auth', 'roles: 2, 5']);
-Route::delete('api/tickets/{tid}/comments/{comment}', 'CommentController@destroy')->middleware(['auth', 'roles: 2, 5']);
+Route::get('api/tickets/{tid}/comments', [CommentController::class, 'index'])->middleware(['auth', 'roles: 2, 5']);
+Route::post('api/tickets/{tid}/comments', [CommentController::class, 'store'])->middleware(['auth', 'roles: 2, 5']);
+Route::get('api/tickets/{tid}/comments/{comment}', [CommentController::class, 'show'])->middleware(['auth', 'roles: 2, 5']);
+Route::put('api/tickets/{tid}/comments/{comment}', [CommentController::class, 'update'])->middleware(['auth', 'roles: 2, 5']);
+Route::delete('api/tickets/{tid}/comments/{comment}', [CommentController::class, 'destroy'])->middleware(['auth', 'roles: 2, 5']);
 
 //Middleware rutas API Post
-Route::get('api/posts', 'PostController@index')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::post('api/posts', 'PostController@store')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::get('api/posts/{post}', 'PostController@show')->middleware(['auth', 'roles: 1, 3, 4, 5']);
-Route::put('api/posts/{post}', 'PostController@update')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::delete('api/posts/{post}', 'PostController@destroy')->middleware(['auth', 'roles: 1, 3, 4, 5']);
+Route::get('api/posts', [PostController::class, 'index'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::post('api/posts', [PostController::class, 'store'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::get('api/posts/{post}', [PostController::class, 'show'])->middleware(['auth', 'roles: 1, 3, 4, 5']);
+Route::put('api/posts/{post}', [PostController::class, 'update'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::delete('api/posts/{post}', [PostController::class, 'destroy'])->middleware(['auth', 'roles: 1, 3, 4, 5']);
 
 //Middleware rutas API Offer
-Route::get('api/offers', 'OfferController@index')->middleware(['auth', 'roles: 1, 5']);
-Route::post('api/offers', 'OfferController@store')->middleware(['auth', 'roles: 1, 4']);
-Route::get('api/offers/{offer}', 'OfferController@show')->middleware(['auth', 'roles: 1, 4, 5']);
-Route::put('api/offers/{offer}', 'OfferController@update')->middleware(['auth', 'roles: 1, 4']);
+Route::get('api/offers', [OfferController::class, 'index'])->middleware(['auth', 'roles: 1, 5']);
+Route::post('api/offers', [OfferController::class, 'store'])->middleware(['auth', 'roles: 1, 4']);
+Route::get('api/offers/{offer}', [OfferController::class, 'show'])->middleware(['auth', 'roles: 1, 4, 5']);
+Route::put('api/offers/{offer}', [OfferController::class, 'update'])->middleware(['auth', 'roles: 1, 4']);
+Route::delete('api/offers/{offer}', [OfferController::class, 'destroy'])->middleware(['auth', 'roles: 1, 4']);
