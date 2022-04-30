ba7d690a506b0e2944c89760cb4bb8e6ac6cd607
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-04-30 10:36:52 +0200

    title fixed

--- laravel/jobnow-backend/resources/views/layouts/app.blade.php
+++ laravel/jobnow-backend/resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">.
+            <div class="container">
                 <a class="navbar-brand">
                     {{ config('app.name', 'jobnow') }}
                 </a>
