dd3e33f883ba9742b2758e19b61aa35c87862ec2
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-05-06 17:42:47 +0200

    api without middleware

--- laravel/jobnow-backend/routes/api.php
+++ laravel/jobnow-backend/routes/api.php
@@ -30,61 +30,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //apiResources
 Route::apiResource('messages', MessageController::class);
 Route::apiResource('tickets', TicketController::class);
-Route::apiResource('tickets/{tid}/comments', CommentController::class)->middleware(['auth', 'roles: 2, 4', 'verified']);
-Route::apiResource('notifications', NotificationController::class)->middleware(['auth', 'verified']);
+Route::apiResource('tickets/{tid}/comments', CommentController::class);
+Route::apiResource('notifications', NotificationController::class);
 Route::apiResource('posts', PostController::class);
 Route::apiResource('offers', OfferController::class);
-Route::apiResource('offers/{oid}/applicatedOffers', ApplicatedOffersController::class)->middleware(['auth', 'roles: 4', 'verified']);
+Route::apiResource('offers/{oid}/applicatedOffers', ApplicatedOffersController::class);
 Route::apiResource('users', UsersApiController::class);
-
-//Middleware rutas API Message
-Route::apiResource('messages', MessageController::class)->only([
-
-    'index', 'show', 'destroy'
-
-])->middleware('auth', 'roles: 1, 3, 4', 'verified');
-
-Route::apiResource('messages', MessageController::class)->only([
-
-    'store', 'update'
-
-])->middleware('auth', 'roles: 1, 4', 'verified');
-
-//Middleware rutas API Ticket
-
-Route::apiResource('tickets', TicketController::class)->only([
-
-    'index'
-
-])->middleware(['auth', 'roles: 1, 2', 'verified']);
-
-Route::apiResource('tickets', TicketController::class)->only([
-
-    'store'
-
-])->middleware(['auth', 'roles: 1, 4', 'verified']);
-
-Route::apiResource('tickets', TicketController::class)->only([
-
-    'show', 'update'
-
-])->middleware(['auth', 'roles: 1, 2, 4', 'verified']);
-
-Route::apiResource('tickets', TicketController::class)->only([
-
-    'destroy'
-
-])->middleware(['auth', 'roles: 1', 'verified']);
-
-//Middleware rutas API Post
-Route::apiResource('posts', PostController::class)->only([
-
-    'index', 'store', 'update'
-
-])->middleware(['auth', 'roles: 1, 4', 'verified']);
-
-Route::apiResource('posts', PostController::class)->only([
-
-    'show', 'destroy'
-
-])->middleware(['auth', 'roles: 1, 2, 4', 'verified']);
