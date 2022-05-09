55fdbbe2d18ab7d5c321347fde6be0df6b148241
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-05-09 15:17:00 +0200

    sidebar in progress

--- laravel/jobnow-backend/resources/views/layouts/app.blade.php
+++ laravel/jobnow-backend/resources/views/layouts/app.blade.php
@@ -20,6 +20,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ asset('images/favicon.ico') }}">
 
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 </head>
 
 <script defer type="text/javascript"> 
@@ -52,12 +58,9 @@
                         @guest
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="dropdown-toggle nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
                                 {{ Auth::user()->name }}
 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                                </svg>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
@@ -83,11 +86,114 @@
                 </div>
             </div>
         </nav>
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Toggle Sidebar</span>
+                    </button>
+        @if (Auth::user()->email_verified_at != null)
+            <div class="wrapper">
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3>Sidebar</h3>
+                </div>
 
-        <main class="py-4">
-            @include('flash')
-            @yield('content')
-        </main>
-    </div>
+                <ul class="list-unstyled components">
+                    <p>MENU</p>
+                    @if (Auth::user()->role_id == 4)
+                        <li class="active">
+                            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="special dropdown-toggle">Home</a>
+                            <ul class="collapse list-unstyled" id="homeSubmenu">
+                                <li>
+                                    <a href="#">Home 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        
+                        <li>
+                            <a href="#">Offers</a>
+                        </li>
+                        <li>
+                            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+                            <ul class="collapse list-unstyled" id="pageSubmenu">
+                                <li>
+                                    <a href="#">Page 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="{{route('premium.index')}}" role="button">Premium</a>
+                        </li>
+                        <li>
+                            <a href="{{route('offers.index')}}" role="button">Offers</a>
+                        </li>
+                        <li>
+                            <a href="{{route('menu.index')}}" role="button">Companies</a>
+                        </li>
+                        <li>
+                            <a href="{{route('posts.index')}}" role="button">Posts</a>
+                        </li>
+                        <li>
+                            <a href="" role="button">Notifications</a>
+                        </li>
+                        <li>
+                            <a href="{{route('tickets.index')}}" role="button">Tickets</a>
+                        </li>
+                        <li>
+                            <a href="{{route('chatapp.index')}}" role="button">Chat</a>
+                        </li>
+                        <li>
+                            <a href="{{route('mynetwork.index')}}" role="button">My Network</a><!-- React -->
+                        </li>
+                    @else
+                        <li>
+                            <a href="{{route('mynetwork.index')}}" role="button">Security</a><!-- React -->
+                        </li>
+                    @endif
+                </ul>
+
+                <!-- <ul class="list-unstyled CTAs">
+                    <li>
+                        <a href="" class="download">button</a>
+                    </li>
+                </ul> -->
+            </nav>
+
+            <div id="content">
+                <main class="py-4">
+                    @include('flash')
+                    @yield('content')
+                </main>
+            </div>
+        </div>
+    @endif
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    });
+    </script>
 </body>
 </html>
+
+
+
+                        
+
