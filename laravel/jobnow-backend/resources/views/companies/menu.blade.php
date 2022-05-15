309da74c53f04803dd7f764532917a72a8af6e82
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-05-15 17:07:10 +0200

    companies menu design changed

--- laravel/jobnow-backend/resources/views/companies/menu.blade.php
+++ laravel/jobnow-backend/resources/views/companies/menu.blade.php
@@ -1,99 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-    .color{  
-        background-color: #6356e5 !important;
-    }
 
+<!DOCTYPE html>
 
-    .bColor{  
-        background-color: #6356e5 !important;
-        border-color: #6356e5 !important;
-    }
-
-	.b2Color{  
-        background-color: #2d0793 !important;
-        border-color: #6356e5 !important;
-    }
-
-    .bsColor{  
-        background-color: #323232 !important;
-        border-color: #323232 !important;
-    }
-</style>
+<head>
+    <link href="{{ asset('css/show.css') }}" rel="stylesheet">
+</head>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    {{ __('Companies') }}
-                </div>
+    <div class="main-body">
+        <h5>
+            Companies you have created ({{\App\Models\Company::where('author_id', '=', Auth::user()->id)->count()}}) /
+            <a class="btn-profile btn btn-primary" href="{{route('companies.create')}}" role="button"> + Add new company</a>
+        </h5>
+        <hr>
+        <div class="col-md-12">
+            @foreach ($companies as $company)
+            <div class="card w-100 mb-3">
                 <div class="card-body">
-                    <br>
-                    <div class="p-2">
-                        <h5>Companies you have created</h5>
-                        <hr>
-                        <div>
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <td scope="col">Name</td>
-                                        <td scope="col">Email</td>
-                                        <td scope="col">Creation Date</td>
-                                        <td scope="col">Options</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($companies as $company)
-                                    <tr>
-                                        <td>{{ $company->name }}</td>
-                                        <td>{{ $company->email }}</td>
-                                        <td>{{ $company->creation_date }}</td>
-                                        <td>
-                                            <a class="w-100 color b2Color btn btn-primary" href="{{route('companies.show', $company)}}" role="button">Show</a>
-                                        </td>
-                                        <td>
-                                            <a class="w-100 color b2Color btn btn-primary" href="{{route('companies.edit', $company)}}" role="button">Edit</a>
-                                        </td>
-                                        <td>
-                                            <button class="w-100 bsColor color btn btn-secondary" id="destroy" type="submit" role="button" data-bs-toggle="modal" data-bs-target="#confirmModal{{$company->id}}">Delete</button>
-
-                                            <!-- Modal -->
-                                            <div class="modal fade" id="confirmModal{{$company->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Delete Company</h5>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            Are you sure you want to delete the company <strong>{{ $company->name }}</strong> ? <br>
-                                                            <span class="text-danger">This action cannot be undone.</span>
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="color bsColor btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                            <form id="form" method="POST" action="{{route('companies.destroy', $company->id)}}">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button id="confirm" type="submit" class="color bColor btn btn-primary">Confirm</button>
-                                                            </form>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!-- End Modal -->                
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            <a class="bColor btn btn-primary" href="{{route('companies.create')}}" role="button"> + Add new company</a>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Full Name</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            {{$company->name}}
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Email</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            {{$company->email}}
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Creation date</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            {{$company->created_at}}
+                        </div>
+                    </div>
+                    <hr>
+                    <a class="w-25 color btn-profile btn btn-primary" href="{{route('companies.show', $company)}}" role="button">Show</a>
+                    <a class="w-25 color btn-profile btn btn-primary" href="{{route('companies.edit', $company)}}" role="button">Edit</a>
+                    <button class="w-25 btn-delete color btn btn-secondary" id="destroy" type="submit" role="button" data-bs-toggle="modal" data-bs-target="#confirmModal{{$company->id}}">Delete</button>
+                    <div class="modal fade" id="confirmModal{{$company->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Delete Company</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure you want to delete the company <strong>{{ $company->name }}</strong> ? <br>
+                                    <span class="text-danger">This action cannot be undone.</span>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="color btn-delete btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <form id="form" method="POST" action="{{route('companies.destroy', $company->id)}}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button id="confirm" type="submit" class="color btn-profile btn btn-primary">Confirm</button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </div>
