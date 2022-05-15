e013870977ba564acc6b4ab46ce3ed4b5e8fac0a
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-05-15 18:50:17 +0200

    admin posts finished

--- laravel/jobnow-backend/app/Http/Controllers/PublicationController.php
+++ laravel/jobnow-backend/app/Http/Controllers/PublicationController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\File;
+use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PublicationController extends Controller
 {
@@ -47,8 +49,19 @@ class PublicationController extends Controller
      * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Post $post)
+    public function destroy(int $id)
     {
-        //
+        $post = Post::all()->where('id', "=", $id)->first();
+        $file = File::where('id', $post->image_id)->first();
+
+        Like::where('post_id', "=", $post->id)->delete();
+
+        $post->delete();
+        $file->delete();
+
+        Storage::disk('public')->delete($file->filename);
+
+        return redirect()->route("publications.index")
+        ->with('success', "The post was deleted successfully.");
     }
 }
