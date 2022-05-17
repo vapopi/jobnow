f3b0a0976ba6c116cedae90d183ee9f0d76fcf33
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-05-17 18:58:54 +0200

    translator added app

--- laravel/jobnow-backend/resources/views/auth/login.blade.php
+++ laravel/jobnow-backend/resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>jobnow</title>
 	<link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+	<!-- <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css"> -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href={{ asset('/css/forms.css') }}>
 	<link rel="stylesheet" href={{ asset('/css/translator.css') }}>
--- laravel/jobnow-backend/resources/views/layouts/app.blade.php
+++ laravel/jobnow-backend/resources/views/layouts/app.blade.php
@@ -25,6 +25,13 @@
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <script defer type="text/javascript">
+		function loadGoogleTranslate() {
+			new google.translate.TranslateElement({
+				pageLanguage: 'en'
+			}, 'google_translate_element');
+		}
+	</script>
 </head>
 
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
@@ -106,6 +113,9 @@
                     <ul class="collapse list-unstyled" id="profile">
                         <br>
                         <li>
+					        <div id="google_translate_element"></div>
+                        </li>
+                        <li>
                             <a class="dropdown-item" href="{{ route('users.show', Auth::user()->id, Auth::user()->avatar_id) }}"">
                                 {{ __('My profile') }}
                             </a>
@@ -152,6 +162,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    	<script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=loadGoogleTranslate"></script>
 </body>
 
 </html>
