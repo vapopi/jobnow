e9d2c2e3cdd48f5987b5f9b79197b249caa2fb52
Sergio Melado Alonso <ser_melado@hotmail.com>
2022-05-17 16:33:20 +0200

    publications.show design

--- laravel/jobnow-backend/resources/views/publications/show.blade.php
+++ laravel/jobnow-backend/resources/views/publications/show.blade.php
@@ -39,31 +39,34 @@
         <div class="w-50 float-end">
             <img class="float-end w-75" src="{{ asset("storage/{$file->filename}") }}" title="post-image"/>
         </div>
-        <hr/>
-        <button id="destroy" type="button" class="btn btn-danger bsColor" data-bs-toggle="modal" data-bs-target="#confirmModal{{ $post->id }}">Delete</button>
 
-		<div class="modal fade" id="confirmModal{{ $post->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLabel">Delete Post</h5>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<div class="modal-body">
-						Are you sure you want to delete the post number <strong>{{ $post->id }}</strong> ? <br>
-						<span class="text-danger">This action cannot be undone.</span>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary bsColor" data-bs-dismiss="modal">Close</button>
-						<form id="form" method="POST" action="{{ route('publications.destroy', $post->id) }}">
-							@csrf
-							@method("DELETE")
-							<button id="confirm" type="submit" class="btn btn-primary bColor">Confirm</button>
-						</form>
-					</div>
+    </div>
+
+    <div class="card-footer">
+        <button id="destroy" type="button" class="btn btn-danger bsColor w-100" data-bs-toggle="modal" data-bs-target="#confirmModal{{ $post->id }}">Delete</button>
+    </div>
+
+	<div class="modal fade" id="confirmModal{{ $post->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Delete Post</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					Are you sure you want to delete the post number <strong>{{ $post->id }}</strong> ? <br>
+					<span class="text-danger">This action cannot be undone.</span>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary bsColor" data-bs-dismiss="modal">Close</button>
+					<form id="form" method="POST" action="{{ route('publications.destroy', $post->id) }}">
+						@csrf
+						@method("DELETE")
+						<button id="confirm" type="submit" class="btn btn-primary bColor">Confirm</button>
+					</form>
 				</div>
 			</div>
 		</div>
-    </div>
+	</div>
 </div>
 @endsection
