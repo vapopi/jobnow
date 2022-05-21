4d367a917329e38faec19b175a8583623d38aa4f
vapopi <vapopi@fp.insjoaquimmir.cat>
2022-05-21 16:18:52 +0200

    change offers create

--- laravel/jobnow-backend/resources/js/components/Offers/Create.js
+++ laravel/jobnow-backend/resources/js/components/Offers/Create.js
@@ -98,106 +98,98 @@ function Create({props}) {
 
             {            
 
-                companies.length === 0 ? (
-                        <li 
-                            className="list-group-item" 
-                            style={{textAlign: "center", color: "white", backgroundColor: "#8c82ec"}}
-                            >You don't have any company.
-                        </li>
-                    ) : 
-                    (
-                    <div className="formContainer shadow mx-auto w-75">
-                        <br/>
-                        <p></p>
-                        <div className="formCreate">
-                            <h4 className='text-center'><strong>Create Offer</strong></h4>
+                <div className="formContainer shadow mx-auto w-75">
+                    <br/>
+                    <p></p>
+                    <div className="formCreate">
+                        <h4 className='text-center'><strong>Create Offer</strong></h4>
 
-                            <div className="w-75 mx-auto">
-                                <div className="form-group">
-                                    <label>Title:</label>
-                                    <input 
-                                        type="text" 
-                                        name="title"
-                                        onChange={handleInputChange} 
-                                        placeholder="Write the title..."
-                                        className="form-control" 
-                                        required/>
-                                </div>
-
-                                <br/>
-                                <div className="form-group">
-                                    <label>Description:</label>
-                                    <input 
-                                        type="text" 
-                                        name="description"
-                                        onChange={handleInputChange} 
-                                        className="form-control" 
-                                        placeholder="Write the description..."
-                                        required/>
-                                </div>
-
-                                <br/>
-                                <p>Choose your company:</p>
-                                <select 
-                                    name="company_id"
-                                    className="selectId w-50 btn btn-secondary btn-block" 
-                                    onChange={handleInputChange}>
-                                    <option>Select option</option>    
-                                    {
-                                        companies.map((key, index) => {
-                                            if(key.author_id == props.userid)
-                                            {
-                                                return(
-                                                    <option key={ index } value={ key.id }>{ key.name }</option>
-                                                )
-                                            }
-                                        })
-                                    }
-                                </select>
-
-                                <br/><br/>
-                                <p>Choose the professional area:</p>
-                                <select 
-                                    name="professional_area_id"
-                                    className="selectId w-50 btn btn-secondary btn-block" 
-                                    onChange={handleInputChange}>
-                                    <option>Select option</option>    
-                                    {
-                                        areas.map(key => (
-                                            <option key={ key.id } value={ key.id }>{ key.name }</option>
-                                        ))
-                                    }
-                                </select>
-
-                                <br/><br/>
-                                {
-                                    state != null ? (
-                                        state.status === 200 ? (
-                                            <span className="text-center text-success">{state.data}</span>
-                                        ):(
-                                            <span className="text-center text-danger">{state.message}</span>
-                                        )
-                                    ):(
-                                        <span className="text-center text-info"></span>
-                                    )
-                                }
-                                <br/><br/>
-
-                                <button 
-                                    type="button" 
-                                    onClick={() => postOffer()} 
-                                    className="mb-3 w-25 btn btn-primary">Send
-                                </button><span> </span>
-
-                                <button 
-                                    type="reset" 
-                                    className="reset mb-3 w-25 btn btn-dark">Reset
-                                </button>
-                                <br/><br/>
+                        <div className="w-75 mx-auto">
+                            <div className="form-group">
+                                <label>Title:</label>
+                                <input 
+                                    type="text" 
+                                    name="title"
+                                    onChange={handleInputChange} 
+                                    placeholder="Write the title..."
+                                    className="form-control" 
+                                    required/>
                             </div>
+
+                            <br/>
+                            <div className="form-group">
+                                <label>Description:</label>
+                                <input 
+                                    type="text" 
+                                    name="description"
+                                    onChange={handleInputChange} 
+                                    className="form-control" 
+                                    placeholder="Write the description..."
+                                    required/>
+                            </div>
+
+                            <br/>
+                            <p>Choose your company:</p>
+                            <select 
+                                name="company_id"
+                                className="selectId w-50 btn btn-secondary btn-block" 
+                                onChange={handleInputChange}>
+                                <option>Select option</option>    
+                                {
+                                    companies.map((key, index) => {
+                                        if(key.author_id == props.userid)
+                                        {
+                                            return(
+                                                <option key={ index } value={ key.id }>{ key.name }</option>
+                                            )
+                                        }
+                                    })
+                                }
+                            </select>
+
+                            <br/><br/>
+                            <p>Choose the professional area:</p>
+                            <select 
+                                name="professional_area_id"
+                                className="selectId w-50 btn btn-secondary btn-block" 
+                                onChange={handleInputChange}>
+                                <option>Select option</option>    
+                                {
+                                    areas.map(key => (
+                                        <option key={ key.id } value={ key.id }>{ key.name }</option>
+                                    ))
+                                }
+                            </select>
+
+                            <br/><br/>
+                            {
+                                state != null ? (
+                                    state.status === 200 ? (
+                                        <span className="text-center text-success">{state.data}</span>
+                                    ):(
+                                        <span className="text-center text-danger">{state.message}</span>
+                                    )
+                                ):(
+                                    <span className="text-center text-info"></span>
+                                )
+                            }
+                            <br/><br/>
+
+                            <button 
+                                type="button" 
+                                onClick={() => postOffer()} 
+                                className="mb-3 w-25 btn btn-primary">Send
+                            </button><span> </span>
+
+                            <button 
+                                type="reset" 
+                                className="reset mb-3 w-25 btn btn-dark">Reset
+                            </button>
+                            <br/><br/>
                         </div>
                     </div>
-                )
+                </div>
+                
             }
         </div>
     );
